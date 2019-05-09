6eb2a0f842899a0340d12c4a8c0d5820310ad5cc
Christian Rohlfing <rohlfing@ient.rwth-aachen.de>
2019-05-09 12:41:57 +0200

    - added ient_signals

---
+++ ient_signals.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# t axis in seconds
+(t, deltat) = np.linspace(-10, 10, 5001, retstep=True)  
+
+# Elementary signals
+gauss     = lambda t: np.exp(-(t)**2)
+unitstep  = lambda t: np.where(t>=0, 1, 0)
+rect      = lambda t: unitstep(t+0.5) - unitstep(t-0.5)
+tri       = lambda t: rect(t/2)*(1-abs(t))
+si        = lambda t: np.sinc(t/np.pi) # English notation sinc(t) = sin(pi t)/(pi t)
+
+eps = np.finfo(float).eps # floating point accuracy
