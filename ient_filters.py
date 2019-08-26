68dee636ad04bba29143c16d44a5dc8427edeefd
Iris Heisterklaus <heisterklaus@ient.rwth-aachen.de>
2019-08-26 09:44:37 +0200

    - added ient_filters to devel branch

---
+++ ient_filters.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy import signal # butter
+
+def butter(cutoff, fs, order=5, type='Tiefpass', fdelta=0):
+    """
+    Design Butterworth filter
+    """
+    btypes = {'Tiefpass':'lowpass', 'Bandpass':'bandpass', 'Hochpass':'highpass'}
+    btype = btypes[type]
+    nyq = 0.5 * fs # Nyquist frequency
+    if btype == 'bandpass':
+        normal_min = (cutoff-fdelta/2) / nyq; normal_max = (cutoff+fdelta/2) / nyq; # normalized min and max frequency
+        normal_cutoff = [normal_min, normal_max]
+    else:
+        normal_cutoff = cutoff / nyq # normalized cutoff-frequency
+    b, a = signal.butter(order, normal_cutoff, btype=btype, analog=False) # coefficients in z-domain
+    return b, a
+    
+# Shortcuts
+def butter_bandpass(f0, fdelta, fs, order=5):
+    b,a = butter(f0, fs, order, 'Bandpass', fdelta)
+    return b, a
+def butter_lowpass(cutoff, fs, order=5):
+    b,a = butter(cutoff, fs, order, 'Tiefpass')
+    return b, a
+def butter_highpass(cutoff, fs, order=5):
+    b,a = butter(cutoff, fs, order, 'Hochpass')
+    return b, a
+
+def butter_filter(s, b, a):
+    """
+    Filter s(n) in z-Domain with filter coefficients a and b:
+                        -1              -M
+            b[0] + b[1]z  + ... + b[M] z
+    G(z) = -------------------------------- S(z)
+                        -1              -N
+            a[0] + a[1]z  + ... + a[N] z
+    """
+    g = signal.lfilter(b, a, s)
+    return g
